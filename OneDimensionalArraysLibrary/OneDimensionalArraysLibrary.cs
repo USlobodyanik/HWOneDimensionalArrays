55d7faec5275ed145b99c701a2d2d3248647656e
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-23 23:00:48 +0200

    Add project files.

---
+++ OneDimensionalArraysLibrary/OneDimensionalArraysLibrary.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace OneDimensionalArrays
+{
+    public class OneDimensionalArraysLibrary
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static int MinimumArrayValue(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int min = array[0];
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static int MinimumArrayValueV2(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            (int minI, int[] array) = MinimumArrayValueIndex(sizeArray, startRandomNumber, lastRandomNumber);
+
+            return array[minI];
+        }
+
+        public static (int, int[]) MinimumArrayValueIndex(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int minI = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[0] > array[i])
+                {
+                    minI = i;
+                }
+            }
+
+            return (minI, array);
+        }
+
+        public static int MaximumArrayValue(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static int MaximumArrayValueV2(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+
+            (int maxI,int[] array) = MaximumArrayValueIndex(sizeArray, startRandomNumber, lastRandomNumber);
+
+            return array[maxI];
+        }
+
+        public static (int, int[]) MaximumArrayValueIndex(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int maxI = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[0] < array[i])
+                {
+                    maxI = i;
+                }
+            }
+
+            return (maxI, array);
+        }
+
+        public static int SumOddIndexArray(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int sumOddI = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sumOddI += array[i];
+                }
+            }
+
+            return sumOddI;
+        }
+
+        public static int[] ReverseArray(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                Swap(ref array[i], ref array[array.Length - 1 - i]);
+            }
+
+            return array;
+        }
+
+        public static int SumOddValueInArray(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int sumOdd = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sumOdd += array[i];
+                }
+            }
+
+            return sumOdd;
+        }
+
+        public static (int[], int[]) HalfReverseArray(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int[] debagArray = new int[array.Length];
+
+            for (int i = 0; i < debagArray.Length; i++)
+            {
+                debagArray[i] = array[i];
+            }
+
+            int startPosition = array.Length / 2 + array.Length % 2;
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                Swap(ref array[i], ref array[startPosition + i]);
+            }
+
+            return (debagArray, array);
+        }
+
+        public static (int[], int[]) BubbleSortAsc(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int[] debagArray = new int[array.Length];
+
+            for (int i = 0; i < debagArray.Length; i++)
+            {
+                debagArray[i] = array[i];
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+
+            return (debagArray, array);
+        }
+
+        public static (int[], int[]) SelectionSortDesc(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = FillingArray(sizeArray, startRandomNumber, lastRandomNumber);
+            int[] debagArray = new int[array.Length];
+
+            for (int i = 0; i < debagArray.Length; i++)
+            {
+                debagArray[i] = array[i];
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int max = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[max] < array[j])
+                    {
+                        max = j;
+                    }
+                }
+
+                Swap(ref array[i], ref array[max]);
+            }
+
+            return (debagArray, array);
+        }
+
+        private static int[] FillingArray(int sizeArray = 5, int startRandomNumber = -10, int lastRandomNumber = 10)
+        {
+            int[] array = new int[sizeArray];
+
+            Random randomNumber = new Random();
+            for (int i = 0; i < sizeArray; i++)
+            {
+                array[i] = randomNumber.Next(startRandomNumber, lastRandomNumber);
+            }
+
+            return array;
+        }
+    }
+}
---
+++ TestArrayConsole/ArrayTestConsole.cs
@@ -0,0 +1,67 @@
+﻿using OneDimensionalArrays;
+using System;
+
+namespace HomeWorkLessonArray
+{
+    class ArrayTestConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Min = {OneDimensionalArraysLibrary.MinimumArrayValue(5, -8, 20)}");
+            Console.WriteLine();
+            Console.WriteLine($"Max = {OneDimensionalArraysLibrary.MaximumArrayValue(5, 0, 20)}");
+            Console.WriteLine();
+
+            (int minI, int[] arrayMin) = OneDimensionalArraysLibrary.MinimumArrayValueIndex(5, -8, 20);
+            for (int i = 0; i < arrayMin.Length; i++)
+            {
+                Console.Write($"{arrayMin[i]} ");
+            }
+            Console.WriteLine($"\tMin value in array indexed = {minI}");
+            Console.WriteLine();
+
+            (int maxI, int[] arrayMax) = OneDimensionalArraysLibrary.MaximumArrayValueIndex(5, -8, 20);
+            for (int i = 0; i < arrayMax.Length; i++)
+            {
+                Console.Write($"{arrayMax[i]} ");
+            }
+            Console.WriteLine($"\tMax value in array indexed = {maxI}");
+            int[] reverseArray = OneDimensionalArraysLibrary.ReverseArray(5, 1, 5);
+            Console.WriteLine();
+
+            Console.Write("Reverse array : ");
+            for (int i = 0; i < reverseArray.Length; i++)
+            {
+                Console.Write($"{reverseArray[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"{Environment.NewLine}Sum odd numbers in array = {OneDimensionalArraysLibrary.SumOddValueInArray(5, 0, 20)}");
+            (int[] debagArray, int[] halfReverseArray) = OneDimensionalArraysLibrary.HalfReverseArray(5, 1, 5);
+            Console.WriteLine();
+
+            Console.WriteLine("Half reverse array : ");
+            for (int i = 0; i < halfReverseArray.Length; i++)
+            {
+                Console.WriteLine($"Input number{debagArray[i]} / reversed {halfReverseArray[i]} ");
+            }
+            Console.WriteLine();
+
+            (int[] debagBubbleArray, int[] bubbleSortArray) = OneDimensionalArraysLibrary.BubbleSortAsc(5, 1, 5);
+            Console.WriteLine("Sort (bubble) array/ascending): ");
+            for (int i = 0; i < bubbleSortArray.Length; i++)
+            {
+                Console.WriteLine($"Input number{debagBubbleArray[i]} / sorted {bubbleSortArray[i]} ");
+            }
+            Console.WriteLine();
+
+            (int[] debagSelectionArray, int[] selectionSortArray) = OneDimensionalArraysLibrary.SelectionSortDesc(5, 1, 5);
+            Console.WriteLine("Sort (Selection) array/descending): ");
+            for (int i = 0; i < selectionSortArray.Length; i++)
+            {
+                Console.WriteLine($"Input number{debagSelectionArray[i]} / sorted {selectionSortArray[i]} ");
+            }
+
+        }
+    }
+}
